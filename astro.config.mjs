fd338bacfe943aa8057660e1475e50c06816c804
Your Namekn20220264 <ksenikic@gmail.com>
2026-08-28 10:20:33 +0200

    domen nulea.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://nulea-bar.netlify.app',
+  site: 'https://www.nulea.me',
   output: 'static',
   trailingSlash: 'always',
 });
